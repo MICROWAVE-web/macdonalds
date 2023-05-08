58d8a2fa3d68785856c66eafe821d3212e9d41eb
playalexei@yandex.ru <www-Sker68>
2023-05-08 16:26:28 +0700

    22

--- macdonalds/static/assets/cartslider/cart.js
+++ macdonalds/static/assets/cartslider/cart.js
@@ -2,14 +2,11 @@ function openNav() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
         window.location.replace("/cart");
     } else {
-        let obj = document.getElementById('mySidenav')
-        if (obj.getBoundingClientRect().width === 0) {
-            //document.getElementById("mySidenav").style.width = "250px";
+        let obj = document.getElementById('mySidenav');
+        if (obj.getBoundingClientRect().left > window.pageXOffset + document.documentElement.clientWidth) {
             obj.classList.add('opened');
             obj.classList.remove('closed');
         } else {
-            //document.getElementById("mySidenav").style.width = "0";
-
             obj.classList.add('closed');
             obj.classList.remove('opened');
         }
@@ -17,12 +14,40 @@ function openNav() {
 }
 
 
+function toCurrency(num) {
+    return new Intl.NumberFormat("ru-RU", {
+        style: "currency",
+        currency: "RUB",
+        minimumFractionDigits: 0,
+    }).format(num);
+}
+
+
+function AddToCart(id, img, name, description, price) {
+    productObj = new Product(id, img, name, description, price);
+    let addedFlag = false;
+    for (let i = 0; i < myCart.products.length; i++) {
+        if (myCart.products[i].id === productObj.id) {
+            myCart.products[i].counter += 1;
+            addedFlag = true;
+        }
+    }
+    if (!addedFlag) {
+        myCart.addProduct(productObj);
+    }
+    localStorage.setItem("cart", JSON.stringify(myCart));
+    cartNum.textContent = myCart.count;
+    console.log(myCart.products)
+}
+
 class Product {
-    constructor(img, name, description, price) {
+    constructor(id, img, name, description, price) {
+        this.id = id;
         this.img = img;
         this.name = name;
         this.description = description;
         this.price = price;
+        this.counter = 1;
     }
 }
 
@@ -34,7 +59,11 @@ class Cart {
     }
 
     get count() {
-        return this.products.length;
+        let value = 0;
+        for (let i = 0; i < myCart.products.length; i++) {
+            value += myCart.products[i].counter;
+        }
+        return value;
     }
 
     addProduct(product) {
@@ -43,6 +72,12 @@ class Cart {
 
     removeProduct(index) {
         this.products.splice(index, 1);
+    }
+
+    removeAll() {
+        this.products = []
+        cartNum.textContent = myCart.count;
+        localStorage.setItem("cart", JSON.stringify(myCart));
     }
 
     get cost() {
@@ -59,7 +94,7 @@ class Cart {
 const myCart = new Cart();
 
 if (localStorage.getItem("cart") == null) {
-  localStorage.setItem("cart", JSON.stringify(myCart));
+    localStorage.setItem("cart", JSON.stringify(myCart));
 }
 const cartNum = document.querySelector("#cart_num");
 const savedCart = JSON.parse(localStorage.getItem("cart"));
